ea4bd2c753e71990886a8e23108ba83cbf7dd7ee
ajju92 <56068907+ajju92@users.noreply.github.com>
2022-03-22 23:19:46 +0530

    1

--- variables.tf
+++ variables.tf
@@ -14,7 +14,6 @@ variable "vpcs" {
   default     = {
     transit = {
       name                                      = "Transit-VPC"
-      cidr                                      = "10.10.0.0/16"
       private_subnets                           = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
       public_subnets                            = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
       flow_log_cloudwatch_log_group_name_prefix = "/aws/vpc-flow-log/transit-vpc/"
