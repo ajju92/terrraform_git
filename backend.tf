497382ecf21c299679559cb67ab6ae985f6bd99b
ajju92 <56068907+ajju92@users.noreply.github.com>
2022-03-22 22:29:10 +0530

    name

--- backend.tf
+++ backend.tf
@@ -4,7 +4,6 @@ terraform {
         bucket  = "stanly-development-terraform-statefiles"
         key     = "terraform/vpc/terraform.tfstate"
         region  = "us-east-1"
-        profile = "default"
-        branch_name = "main"
+        profile = "default"      
     }
 }
